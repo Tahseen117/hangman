2973dc4cd22509cca3a2578c7cc436cf377b4b38
Tahseen117 <tahskhan117@gmail.com>
2022-06-05 19:06:40 +0530

    initial code

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+import  os
+from ascii_art import art , hangman
+from wordlist import word_list
+
+
+word = random.choice(word_list)
+lost=0
+os.system('cls')
+print(hangman)
+
+display = []
+for position in range(len(word)):
+    display += "_"
+print(art[0])
+print("Number of letters in the chosen word: {}".format(display))
+
+p=0
+while "_" in display:
+    guess = input ("Guess the letter :")
+    for i in range(len(word)):
+        if word[i] == guess:
+            display[i] = word[i]
+    
+    
+    os.system('cls')
+
+    print(hangman)
+    if guess not in word:
+        if p == len(art)-2:
+            lost=1
+            print("You lose")
+            print(art[p+1])
+            break
+        print(art[p+1])
+        p+=1
+    else:
+        print(art[p])
+    
+    print (display)
+
+if lost != 1:
+    print("YOU WON !!")
+
